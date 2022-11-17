a93cb719a943d8b12adb475560442e9de7aeba9e
Mike <justeivin227@gmail.com>
2022-11-17 15:36:06 +0100

    Add function to mark cells with symbols Game now also switches players after every turn.

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
 const gameBoard = (() => {
-    const cellArray = [null, null, 'o', null, null, 'x', null, null, null];
-    const getCellContent = (cellIndex) => {return cellArray[cellIndex]}
-    const markCell = (symbol, cellIndex) => {cellArray[cellIndex] = symbol}
+    let cellArray = [null, null, null, null, null, null, null, null, null];
+    const getCellContent = (cellIndex) => { return cellArray[cellIndex] }
+    const markCell = (symbol, cellIndex) => { cellArray[cellIndex] = symbol }
     const checkIfEmpty = (cellIndex) => {
-        if(cellArray[cellIndex] == null) {
+        if (cellArray[cellIndex] == null) {
             return true;
         } else return false;
     }
     const resetBoard = () => {
-        cellArray.forEach(cell => {
-            cell = null;
-        })
+        for(let i = 0; i < cellArray.length; i++) {
+            cellArray[i] = null;
+        }
+        displayController.updateGameBoard();
     }
 
-    return {getCellContent, markCell, checkIfEmpty, resetBoard};
+    return {getCellContent, markCell, checkIfEmpty, resetBoard };
 })();
 
 const Player = (symbol) => {
     let score = 0;
-    const getScore = () => {return score}
-    const setScore = (points) => {score += points}
-    const getSymbol = () => {return symbol}
+    const getScore = () => { return score }
+    const setScore = (points) => { score += points }
+    const getSymbol = () => { return symbol }
 
-    return {getScore, setScore, getSymbol};
+    return { getScore, setScore, getSymbol };
 }
 
 const displayController = (() => {
@@ -33,5 +34,33 @@ const displayController = (() => {
         })
     }
 
+    cells.forEach((cell, cellIndex) => {
+        cell.addEventListener('click', () => {
+            gameController.play(cellIndex);
+        });
+    })
+
     return {updateGameBoard}
+})();
+
+const gameController = (() => {
+    let player1 = Player('O');
+    let player2 = Player('X');
+    let turnOwner = player1;
+    const getTurnOwner = () => { return turnOwner }
+    const switchTurn = () => {
+        if (turnOwner == player1) {
+            turnOwner = player2;
+        } else turnOwner = player1;
+    }
+
+    const play = (cellIndex) => {
+        if (gameBoard.checkIfEmpty(cellIndex)) {
+            gameBoard.markCell(getTurnOwner().getSymbol(), cellIndex);
+            displayController.updateGameBoard()
+            switchTurn();
+        }
+    }
+
+    return {play}
 })();
